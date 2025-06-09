d2646cc3e194cbbb30bdde18758bd9db6719d8d9
Alfin Saputra <alfinkamal007@gmail.com>
2025-06-09 17:46:59 +0700

    tambah detail produk

--- debugging/FormGUI/DetailProduk.Designer.cs
+++ debugging/FormGUI/DetailProduk.Designer.cs
@@ -32,53 +32,110 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(29, 41);
+            pictureBox1.Location = new Point(44, 96);
+            pictureBox1.Margin = new Padding(4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(245, 210);
+            pictureBox1.Size = new Size(306, 262);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(328, 41);
+            label1.Location = new Point(410, 96);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(120, 25);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Detail produk";
+            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(328, 114);
+            label2.Location = new Point(410, 143);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(57, 25);
             label2.TabIndex = 2;
-            label2.Text = "label2";
+            label2.Text = "harga";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(328, 180);
+            label3.Location = new Point(410, 199);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(82, 25);
             label3.TabIndex = 3;
-            label3.Text = "label3";
+            label3.Text = "deskripsi";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(32, 33, 36);
+            button1.Font = new Font("Poppins ExtraLight", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(410, 307);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 60);
+            button1.TabIndex = 4;
+            button1.Text = "keranjang";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(32, 33, 36);
+            button2.Font = new Font("Poppins ExtraLight", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(609, 307);
+            button2.Name = "button2";
+            button2.Size = new Size(173, 60);
+            button2.TabIndex = 5;
+            button2.Text = "Beli";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(32, 33, 36);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 50);
+            panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(32, 33, 36);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 512);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1000, 50);
+            panel2.TabIndex = 7;
             // 
             // DetailProduk
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
+            Margin = new Padding(4);
             Name = "DetailProduk";
             Text = "DetailProduk";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -92,5 +149,9 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
